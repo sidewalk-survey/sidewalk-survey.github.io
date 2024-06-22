37dc51d5266993413616bb781a3115b43c69456a
rrrrrrockpang <ypang2@cs.washington.edu>
2024-06-22 17:50:43 -0400

    dynamic Q5

--- src/survey/Questions/Question5.js
+++ src/survey/Questions/Question5.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import TextQuestion from '../../components/TextQuestion'; 
 
-const Question5 = ({ previousStep, nextStep, handleChange, stepNumber }) => {
+const Question5 = ({ stepNumber, nextStep, previousStep, answers, handleChange }) => {
+  const mobilityAid = answers.mobilityAid;
+
   return (
     <TextQuestion
-      questionText={`${stepNumber}. When using your current mobility aid, what are the most difficult sidewalk barriers that you encounter?`}
+      questionText={<span>{stepNumber}. When using your <strong>{mobilityAid}</strong>, what are the most difficult sidewalk barriers that you encounter?</span>}
       inputId="sidewalkBarriers"
       placeholderText="e.g. missing curb ramps, poles..."
       handleChange={handleChange}
       previousStep={previousStep}
       nextStep={nextStep}
     />
+
   );
+
+
 };
 
 export default Question5;
