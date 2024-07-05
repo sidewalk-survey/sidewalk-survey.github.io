f4d160955406db3ae387e4937e7cd2a58f148fd5
crescendochu <chuchuli@uw.edu>
2024-07-05 15:40:05 -0400

    Moved the crops data into a new file

---
+++ src/survey/CropsData/group0CropsData.js
@@ -0,0 +1,118 @@
+// group0CropsData.js
+
+const group0CropsData = [
+    {
+        "City": "seattle",
+        "LabelID": 111698,
+        "LabelTypeID": 4,
+        "PanoID": "8UyaKl2zh2BE6ZwRTCxkyA",
+        "Heading": 227.6875,
+        "Pitch": -35,
+        "Zoom": 1,
+        "FOV": 90,
+        "CanvasX": 326,
+        "CanvasY": 216
+    },
+    {
+        "City": "seattle",
+        "LabelID": 205972,
+        "LabelTypeID": 4,
+        "PanoID": "EwjRCeSEVWFQGRnqsXM_DA",
+        "Heading": 205.7209777832,
+        "Pitch": -22.125,
+        "Zoom": 1,
+        "FOV": 90,
+        "CanvasX": 486,
+        "CanvasY": 192
+    },
+  
+    {
+      "City": "seattle",
+      "LabelID": 194344,
+      "LabelTypeID": 4,
+      "PanoID": "JWaaynPQwfIX3QmAPpA8gg",
+      "Heading": 210.3660736084,
+      "Pitch": -6.4642858505,
+      "Zoom": 2,
+      "FOV": 45,
+      "CanvasX": 536,
+      "CanvasY": 242
+  },
+  {
+      "City": "seattle",
+      "LabelID": 208040,
+      "LabelTypeID": 4,
+      "PanoID": "YDaym1DloFZnBdGoaDUyDQ",
+      "Heading": 311.375,
+      "Pitch": -35,
+      "Zoom": 1,
+      "FOV": 90,
+      "CanvasX": 448,
+      "CanvasY": 122
+  },
+  
+  {
+    "City": "seattle",
+    "LabelID": 107450,
+    "LabelTypeID": 4,
+    "PanoID": "ibbpox2xm6Y8ZhdcWYTMHw",
+    "Heading": 220.9821472168,
+    "Pitch": -20.0892848969,
+    "Zoom": 2,
+    "FOV": 45,
+    "CanvasX": 463,
+    "CanvasY": 172
+  },
+  
+  {
+    "City": "seattle",
+    "LabelID": 169928,
+    "LabelTypeID": 3,
+    "PanoID": "o0Uq8GMm_BMCOxaKE1FYoQ",
+    "Heading": 161.5892791748,
+    "Pitch": -35,
+    "Zoom": 1,
+    "FOV": 90,
+    "CanvasX": 216,
+    "CanvasY": 146
+  },
+  {
+    "City": "seattle",
+    "LabelID": 139731,
+    "LabelTypeID": 3,
+    "PanoID": "tE85ocVCFwcoXIbDpQRVDQ",
+    "Heading": 7.8656058311,
+    "Pitch": -35,
+    "Zoom": 1,
+    "FOV": 90,
+    "CanvasX": 318,
+    "CanvasY": 203
+  },
+  
+  {
+    "City": "seattle",
+    "LabelID": 100492,
+    "LabelTypeID": 3,
+    "PanoID": "Ml-OCQTY1s5k7b6DSxHZ-A",
+    "Heading": 160.5535736084,
+    "Pitch": -17.0982151031,
+    "Zoom": 2,
+    "FOV": 45,
+    "CanvasX": 193,
+    "CanvasY": 190
+  },
+  {
+  "City": "seattle",
+  "LabelID": 191782,
+  "LabelTypeID": 3,
+  "PanoID": "tSDTx7aNmeGUosgKkWJC8Q",
+  "Heading": 65.796875,
+  "Pitch": -3.609375,
+  "Zoom": 3,
+  "FOV": 23,
+  "CanvasX": 361,
+  "CanvasY": 328
+  },
+  ];
+
+  export default group0CropsData;
--- src/survey/SurveyForm/SurveyForm.js
+++ src/survey/SurveyForm/SurveyForm.js
@@ -16,6 +16,7 @@ import EndingPage from '../StartEndPages/EndingPage';
 import ContinuePage from '../Questions/ContinuePage';
 import { v4 as uuidv4 } from 'uuid';
 import RankQuestion from '../Questions/RankQuestion';
+import group0CropsData from '../CropsData/group0CropsData';
 
 // Firebase configuration
 const firebaseConfig = {
@@ -100,9 +101,7 @@ const SurveyComponent = () => {
           }
 
           setCurrentStep(MOBILITYAID_STEP);
-          // setSurfaceSelection(data.surfaceSelection || { surfaceA: [], surfaceB: [] });
-          // setObstacleSelection(data.obstacleSelection || { obstacleA: [], obstacleB: [] });
-          // setNoCurbSelection(data.noCurbSelection || { noCurbA: [], noCurbB: [] });
+
           setImageSelections(data.imageSelections || {
             group0: { group0A: [], group0B: [] },
             group1: { group1A: [], group1B: [] },
@@ -123,122 +122,8 @@ const SurveyComponent = () => {
     fetchData();
   }, [id]);
 
-const group0CropsData = [
-  {
-      "City": "seattle",
-      "LabelID": 111698,
-      "LabelTypeID": 4,
-      "PanoID": "8UyaKl2zh2BE6ZwRTCxkyA",
-      "Heading": 227.6875,
-      "Pitch": -35,
-      "Zoom": 1,
-      "FOV": 90,
-      "CanvasX": 326,
-      "CanvasY": 216
-  },
-  {
-      "City": "seattle",
-      "LabelID": 205972,
-      "LabelTypeID": 4,
-      "PanoID": "EwjRCeSEVWFQGRnqsXM_DA",
-      "Heading": 205.7209777832,
-      "Pitch": -22.125,
-      "Zoom": 1,
-      "FOV": 90,
-      "CanvasX": 486,
-      "CanvasY": 192
-  },
-
-  {
-    "City": "seattle",
-    "LabelID": 194344,
-    "LabelTypeID": 4,
-    "PanoID": "JWaaynPQwfIX3QmAPpA8gg",
-    "Heading": 210.3660736084,
-    "Pitch": -6.4642858505,
-    "Zoom": 2,
-    "FOV": 45,
-    "CanvasX": 536,
-    "CanvasY": 242
-},
-{
-    "City": "seattle",
-    "LabelID": 208040,
-    "LabelTypeID": 4,
-    "PanoID": "YDaym1DloFZnBdGoaDUyDQ",
-    "Heading": 311.375,
-    "Pitch": -35,
-    "Zoom": 1,
-    "FOV": 90,
-    "CanvasX": 448,
-    "CanvasY": 122
-},
-
-{
-  "City": "seattle",
-  "LabelID": 107450,
-  "LabelTypeID": 4,
-  "PanoID": "ibbpox2xm6Y8ZhdcWYTMHw",
-  "Heading": 220.9821472168,
-  "Pitch": -20.0892848969,
-  "Zoom": 2,
-  "FOV": 45,
-  "CanvasX": 463,
-  "CanvasY": 172
-},
-
-{
-  "City": "seattle",
-  "LabelID": 169928,
-  "LabelTypeID": 3,
-  "PanoID": "o0Uq8GMm_BMCOxaKE1FYoQ",
-  "Heading": 161.5892791748,
-  "Pitch": -35,
-  "Zoom": 1,
-  "FOV": 90,
-  "CanvasX": 216,
-  "CanvasY": 146
-},
-{
-  "City": "seattle",
-  "LabelID": 139731,
-  "LabelTypeID": 3,
-  "PanoID": "tE85ocVCFwcoXIbDpQRVDQ",
-  "Heading": 7.8656058311,
-  "Pitch": -35,
-  "Zoom": 1,
-  "FOV": 90,
-  "CanvasX": 318,
-  "CanvasY": 203
-},
-
-{
-  "City": "seattle",
-  "LabelID": 100492,
-  "LabelTypeID": 3,
-  "PanoID": "Ml-OCQTY1s5k7b6DSxHZ-A",
-  "Heading": 160.5535736084,
-  "Pitch": -17.0982151031,
-  "Zoom": 2,
-  "FOV": 45,
-  "CanvasX": 193,
-  "CanvasY": 190
-},
-{
-"City": "seattle",
-"LabelID": 191782,
-"LabelTypeID": 3,
-"PanoID": "tSDTx7aNmeGUosgKkWJC8Q",
-"Heading": 65.796875,
-"Pitch": -3.609375,
-"Zoom": 3,
-"FOV": 23,
-"CanvasX": 361,
-"CanvasY": 328
-},
-];
-
   // for handling the case where the image group is empty
+  
   useEffect(() => {
     if (currentStep === 7 && imageSelections.group0.group0A.length < 1) {
       setCurrentStep(8); // Skip Group 0 ImageComparison A if no images
