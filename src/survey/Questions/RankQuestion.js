299f735e2deba9540a26245980422df7e7fdea4e
crescendochu <chuchuli@uw.edu>
2024-07-13 13:34:32 -0400

    Shortened rank question

--- src/survey/Questions/RankQuestion.js
+++ src/survey/Questions/RankQuestion.js
@@ -2,20 +2,15 @@ import React, { useState, useEffect } from 'react';
 import DraggableQuestion from '../../components/DraggableQuestion';
 
 const rankOptions = [
-    { id: '1', value: "Fire hydrant or utility pole on sidewalk" },
-    { id: '2', value: "Tree or vegetation on sidewalk" },
-    { id: '3', value: "Parked scooters, bikes, or cars on sidewalk" },
-    { id: '4', value: "Uneven sidewalk panel" },
-    { id: '5', value: "Broken surface or cracks on sidewalk" },
-    { id: '6', value: "Utility panel on sidewalk" },
-    { id: '7', value: "Brick or cobblestone surface" },
-    { id: '8', value: "Grass surface" },
-    { id: '9', value: "Sand or gravel surface" },
-    { id: '10', value: "Narrow sidewalk (width less than 1.2m / 4 ft)" },
-    { id: '11', value: "Missing curb ramp at intersections" },
-    { id: '12', value: "Steep slope (sidewalk incline greater than 5%)" },
-    { id: '13', value: "Steps or stairs" },
-    { id: '14', value: "Trash or recycling bins on sidewalk" },
+    { id: '1', value: "Uneven sidewalk panels" },
+    { id: '2', value: "Broken surface or cracks on sidewalk" },
+    { id: '3', value: "Utility panels on sidewalk" },
+    { id: '4', value: "Brick or cobblestone surface" },
+    { id: '5', value: "Grass surface" },
+    { id: '6', value: "Sand or gravel surface" },
+    { id: '7', value: "Narrow sidewalk (width less than 1.2m / 4 ft)" },
+    { id: '8', value: "Missing curb ramp at intersections" },
+    { id: '9', value: "Steep slope (sidewalk incline greater than 5%)" },
   ];
 
 const RankQuestion = ({ stepNumber, nextStep, previousStep, answers, updateAnswers }) => {
