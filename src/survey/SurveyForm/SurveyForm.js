ea8e6c7b6b075b5cf13a1ba6c6ee446b7d2faecd
crescendochu <chuchuli@uw.edu>
2024-07-22 12:43:02 -0400

    Updated to responsive units

--- src/survey/SurveyForm/SurveyForm.js
+++ src/survey/SurveyForm/SurveyForm.js
@@ -131,7 +131,7 @@ const SurveyComponent = () => {
   const progressValue = (currentStep / totalSteps) * 100;
 
   const startSurvey = () => {
-    setCurrentStep(1); // Start the survey
+    setCurrentStep(8); // Start the survey
     setStartTime(new Date());
   };
   
