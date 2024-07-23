da08757addfdf9f88920627b04b9bd57401b180d
crescendochu <chuchuli@uw.edu>
2024-07-23 13:41:51 -0400

    Fixed #83

--- src/survey/StartEndPages/EndingPage.js
+++ src/survey/StartEndPages/EndingPage.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
-import { Button, Card, CardBody,  } from "@material-tailwind/react";
+import React, { useState } from 'react';
+import { Button } from "@material-tailwind/react";
+import './EndingPage.css';
 
 const EndingPage = ({ previousStep, onSubmit, continueUrl, onEmailLink }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -7,53 +8,65 @@ const EndingPage = ({ previousStep, onSubmit, continueUrl, onEmailLink }) => {
   const handleSubmit = () => {
     setIsSubmitting(true);
     onSubmit();
-  }
+  };
 
   return (
-    
-    <div className="max-w-6xl flex flex-col items-center justify-center p-4 gap-2">
-      <h2>Thank you so much for completing this survey!</h2>
-      {!continueUrl &&(
-      <div className="max-w-md mb-8">
-        <img src={`${process.env.PUBLIC_URL}/img/access-score.png`} alt="Welcome" className="w-full" />
+    <div className="ending-page-container">
+      <div className="ending-page-image-container">
+        {!continueUrl && (
+          <img 
+            src={`${process.env.PUBLIC_URL}/img/access-score.png`} 
+            alt="Welcome" 
+            className="w-full h-full" 
+          />
+        )}
       </div>
-    )}
-      {!continueUrl &&(
-        <p style={{fontSize: '1em', lineHeight: '1.3'}}>Your responses will be used to inform <b>personalized routing</b> algorithms and maps for <b>people who use mobility devices.</b></p>
-      )}
-      {continueUrl && (
-        <div>
-          <p>You can resume your survey later using the following link:</p>
-          <a href={continueUrl} className="text-cyan-800 underline">{continueUrl}</a>
-        </div>
-      )}
-      <div className="question-button-group flex flex-col">
-      <div className="flex justify-center gap-4 mt-5">
-      {continueUrl && (
+      <div className="ending-page-right-container">
+      <div className="ending-page-text-container">
+        <h2>Thank you so much for completing this survey!</h2>
+        {!continueUrl && (
+          <p className="text-left" style={{ fontSize: '1em', lineHeight: '1.3' }}>
+            Your responses will be used to inform <b>personalized routing</b> algorithms and maps for <b>people who use mobility devices.</b>
+          </p>
+        )}
+        {continueUrl && (
+          <div className="text-right">
+            <p>You can resume your survey later using the following link:</p>
+            <a href={continueUrl} className="text-cyan-800 underline">{continueUrl}</a>
+          </div>
+        )}
+        <div className="flex flex-col items-start gap-4">
+          {continueUrl && (
+            <Button
+              className='lg-font-size-button'
+              color="teal"
+              variant="outlined"
+              size="lg"
+              onClick={onEmailLink}
+            >
+              Email Me the Link
+            </Button>
+          )}
           <Button
             className='lg-font-size-button'
             color="teal"
-            variant= "outlined"
             size="lg"
-            onClick={onEmailLink}
+            onClick={handleSubmit}
+            disabled={isSubmitting}
           >
-            Email Me the Link
+            Submit
           </Button>
-        )}
-      <Button
-        className='lg-font-size-button'
-        color="teal"
-        size="lg"
-        onClick={handleSubmit}
-        disabled={isSubmitting}
-      >Submit
-        </Button>
-      {/* <span className="text-w text-teal-700">press Enter ↵</span> */}
         </div>
       </div>
+      <div className='ending-page-contact-info'>
+            <p>For any questions or concerns, please contact:</p>
+            <p className="text-cyan-800">
+              <a href="mailto:chuchuli@cs.washington.edu" className="text-teal-700 underline">chuchuli@cs.washington.edu</a>.
+              </p>
+      </div>
+      </div>
     </div>
-
-);
+  );
 };
 
 export default EndingPage;
